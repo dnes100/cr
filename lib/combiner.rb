5801e631e9f353f2f530d54b72c22b47dd253094
Dinesh Hyaunmikha <dinesh.hyaunmikha@gmail.com>
2018-04-18 20:36:29 +0300

    used attr_accessor, changed var done to in_progress for readablity, moved some logic to private methods

--- lib/combiner.rb
+++ lib/combiner.rb
@@ -4,23 +4,21 @@
 # - block combining two elements having the same key or a single element, if there is no partner
 # output:
 # - enumerator for the combined elements
+#
 class Combiner
+  attr_accessor :key_extractor
 
 	def initialize(&key_extractor)
-		@key_extractor = key_extractor
-	end
-
-	def key(value)
-		value.nil? ? nil : @key_extractor.call(value)
+    self.key_extractor = key_extractor
 	end
 
 	def combine(*enumerators)
 		Enumerator.new do |yielder|
 			last_values = Array.new(enumerators.size)
-			done = enumerators.all? { |enumerator| enumerator.nil? }
-			while not done
+      in_progress = enumerators.any?
+			while in_progress
 				last_values.each_with_index do |value, index|
-					if value.nil? and not enumerators[index].nil?
+          if value.nil? && enumerators[index]
 						begin
 							last_values[index] = enumerators[index].next
 						rescue StopIteration
@@ -29,19 +27,9 @@ class Combiner
 					end
 				end
 
-				done = enumerators.all? { |enumerator| enumerator.nil? } and last_values.compact.empty?
-				unless done
-					min_key = last_values.map { |e| key(e) }.min do |a, b|
-						if a.nil? and b.nil?
-							0
-						elsif a.nil?
-							1
-						elsif b.nil?
-							-1
-						else
-							a <=> b
-						end
-					end
+        in_progress = enumerators.any? || !last_values.compact.empty?
+				if in_progress
+          min_key = get_min_key(last_values)
 					values = Array.new(last_values.size)
 					last_values.each_with_index do |value, index|
 						if key(value) == min_key
@@ -54,4 +42,28 @@ class Combiner
 			end
 		end
 	end
+
+  private
+
+	def key(value)
+    return if value.nil?
+
+		key_extractor.call(value)
+	end
+
+  def get_min_key(values)
+    keys = values.map { |e| key(e) }
+
+    keys.min do |a, b|
+      if a.nil? && b.nil?
+        0
+      elsif a.nil?
+        1
+      elsif b.nil?
+        -1
+      else
+        a <=> b
+      end
+    end
+  end
 end
